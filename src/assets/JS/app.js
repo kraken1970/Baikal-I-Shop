84dd43223bdd6446273b4232641fe610c31a6b09
kraken1970 <kraken@hi.ru>
2018-02-16 01:02:40 +0300

    lesson 5 ok

--- src/assets/JS/app.js
+++ src/assets/JS/app.js
@@ -1,8 +1,22 @@
-$(function () {
-  "use strickt";
+"use strickt";
 
-  $('.title_box').click(function () {
-    $(this).toggleClass('open');
-    $(this).next('.list_link').toggleClass('open');
-  });
-});
+// $(function () {
+  
+//   $('.title_box').click(function () {
+//     $(this).toggleClass('open');
+//     $(this).next('.list_link').toggleClass('show');
+//   });
+// });
+
+(function () {
+ let titleBox = document.querySelectorAll('.title_box');
+ let listLink = document.querySelectorAll('.list_link');
+ for (let i = 0; i < titleBox.length; i++) {
+   titleBox[i].addEventListener('click', function (evt) {
+    evt.preventDefault();
+    titleBox[i].classList.toggle('open');
+    listLink[i].classList.toggle('show');
+   });
+ }
+})();
+
