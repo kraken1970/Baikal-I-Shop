b0654005878068cff0e7984b17f04781ee3287a6
kraken1970 <kraken@hi.ru>
2018-02-15 15:02:12 +0300

    lesson 4 ok

--- src/assets/JS/app.js
+++ src/assets/JS/app.js
@@ -1,1 +1,8 @@
-console.log('HI!!!!!!!!!!');
+$(function () {
+  "use strickt";
+
+  $('.title_box').click(function () {
+    $(this).toggleClass('open');
+    $(this).next('.list_link').toggleClass('open');
+  });
+});
