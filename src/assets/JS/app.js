38d1cd14bd3b10a02c9217e738a0106ccdf048ee
kraken1970 <kraken@hi.ru>
2018-02-17 01:43:25 +0300

    lesson 6 ok . add jquery

--- src/assets/JS/app.js
+++ src/assets/JS/app.js
@@ -1,13 +1,5 @@
 "use strickt";
 
-// $(function () {
-  
-//   $('.title_box').click(function () {
-//     $(this).toggleClass('open');
-//     $(this).next('.list_link').toggleClass('show');
-//   });
-// });
-
 (function () {
  let titleBox = document.querySelectorAll('.title_box');
  let listLink = document.querySelectorAll('.list_link');
@@ -19,4 +11,24 @@
    });
  }
 })();
+
+$(function () {
+  
+  $('#slider_price').slider({
+    max: 1000,
+    min: 0,
+    range: true,
+    values: [190,728],
+    slide: function( event, ui ) {
+      $('input[name="minPrice"]').val( '$' +  ui.values[0] );
+      $('input[name="maxPrice"]').val( '$' +  ui.values[1] );
+    }
+  });
+
+  $('input[name="minPrice"]').val( '$' +
+    $('#slider_price').slider('values',0) );
+  
+  $('input[name="maxPrice"]').val( '$' +
+    $('#slider_price').slider('values',1) );
+});
 
