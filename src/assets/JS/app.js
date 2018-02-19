cee267f420ccad9972141d96ba780271e6279ace
kraken1970 <kraken@hi.ru>
2018-02-19 16:36:36 +0300

    lesson 9 ok

--- src/assets/JS/app.js
+++ src/assets/JS/app.js
@@ -1,5 +1,5 @@
 "use strickt";
-
+// вложенные подменю category
 (function () {
  let titleBox = document.querySelectorAll('.title_box');
  let listLink = document.querySelectorAll('.list_link');
@@ -13,7 +13,7 @@
 })();
 
 $(function () {
-  
+  // двухпозиционный бегунок с инпутами
   $('#slider_price').slider({
     max: 1000,
     min: 0,
@@ -31,15 +31,27 @@ $(function () {
   $('input[name="maxPrice"]').val( '$' +
     $('#slider_price').slider('values',1) );
 
+// поле корзины
   $('#cart, .title_cart').click(function () {
     $('#cart_box').toggleClass('open');
   });
-
+// форма входа
   $('#sign').click(function () {
     $('#black_fill, #modal').toggleClass('open');
   });
   $('#close_mod, #black_fill').click(function () {
     $('#black_fill, #modal').toggleClass('open');
   });
+
+  // поле search
+  $('#search_button').click(function () {
+    $('#search_panel').toggleClass('open');
+  });
+
+  // дозагрузка товаров
+  $('#load_more').click(function (event) {
+    event.preventDefault();
+    $('.list_product').append('<a href="#" class="product box main_flex__nowrap flex__jcontent_center flex__align-items_center"><div class="img_product"><img src="../assets/images/product/04.png" alt=""></div><h2>Our Legacy Splash Mid Sleeve Tee</h2><p>Black Grey Plants</p><p class="price">$90</p></a><a href="#" class="product box main_flex__nowrap flex__jcontent_center flex__align-items_center"><div class="img_product"><img src="../assets/images/product/04.png" alt=""></div><h2>Our Legacy Splash Mid Sleeve Tee</h2><p>Black Grey Plants</p><p class="price">$90</p></a><a href="#" class="product box main_flex__nowrap flex__jcontent_center flex__align-items_center"><div class="img_product"><img src="../assets/images/product/04.png" alt=""></div><h2>Our Legacy Splash Mid Sleeve Tee</h2><p>Black Grey Plants</p><p class="price">$90</p></a>')
+  }); 
 });
 
